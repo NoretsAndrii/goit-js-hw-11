fb78e2c98178c04c0b568b36209c9802407fd422
NoretsAndrii <147941173+NoretsAndrii@users.noreply.github.com>
2024-03-03 21:06:54 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const formForSearch = document.querySelector('.form');
 const inputFormForSearch = document.querySelector('.form-input');
 const btnForSearch = document.querySelector('.form-btn');
@@ -7,9 +12,10 @@ formForSearch.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(event) {
   event.preventDefault();
-  gallary.innerHTML = '';
   const wordForSearch = inputFormForSearch.value.trim();
   if (!wordForSearch) return;
+  gallary.innerHTML = '<span class="loader"></span>';
+
   console.log('good');
 
   const searchParams = new URLSearchParams({
@@ -30,34 +36,58 @@ function handlerSubmit(event) {
     .then(resultOfSearch => {
       console.log('very good');
       console.log(resultOfSearch);
-      if (resultOfSearch.hits.length === 0)
-        return console.log(
-          '"Sorry, there are no images matching your search query. Please try again!"'
-        );
-      createContent(resultOfSearch);
-      //   const markup = users
-      //     .map(user => {
-      //       return `<li>
-      //           <p><b>Name</b>: ${user.name}</p>
-      //           <p><b>Email</b>: ${user.email}</p>
-      //           <p><b>Company</b>: ${user.company.name}</p>
-      //         </li>`;
-      //     })
-      //     .join('');
+      if (resultOfSearch.hits.length === 0) {
+        gallary.innerHTML = '';
+        inputFormForSearch.value = '';
 
-      //   userList.insertAdjacentHTML('beforeend', markup);
+        return iziToast.error({
+          close: false,
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+          progressBar: false,
+        });
+      }
+      gallary.innerHTML = createContent(resultOfSearch);
+      inputFormForSearch.value = '';
+      const lightbox = new SimpleLightbox('.gallary-item-link', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
+      lightbox.refresh();
     })
     .catch(error => console.log(error));
 }
 
 function createContent({ hits: arrImages }) {
-  console.log(arrImages);
-  const content = arrImages
+  return arrImages
     .map(
       image =>
-        `<li class="gallary-item"><img class="gallary-image" src="${image.webformatURL}" alt="${image.tags}"></li>`
+        `<li class="gallary-item">
+      <a class="gallary-item-link" href="${image.largeImageURL}"><img
+        class="gallary-image"
+        src="${image.webformatURL}"
+        alt="${image.tags}"
+      /></a>
+         <ul class="info-list">
+          <li class="info-list-item">
+            <h2 class="list-item-title">Likes</h2>
+            <p class="list-item-info">${image.likes}</p>
+          </li>
+          <li class="info-box-list-item">
+            <h2 class="list-item-title">Views</h2>
+            <p class="list-item-info">${image.views}</p>
+          </li>
+          <li class="info-box-list-item">
+            <h2 class="list-item-title">Comments</h2>
+            <p class="list-item-info">${image.comments}</p>
+          </li>
+          <li class="info-box-list-item">
+            <h2 class="list-item-title">Downloads</h2>
+            <p class="list-item-info">${image.downloads}</p>
+          </li>
+        </ul>
+       </li>`
     )
     .join('');
-  console.log(content);
-  gallary.insertAdjacentHTML('beforeend', content);
 }
